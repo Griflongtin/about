88cf7933aa51d63be661daa64f94159f4eb5e6af
Griffin P Longtin and Holly Wilkalis <holly@>
2018-02-08 08:05:24 -0800

    init first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import ReactDOM from "react-dom";
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      google
+      <Switch>
+      </Switch>
+
+    </div>
+  );
+}
+
+export default App;
